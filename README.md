e44cfd7646ae569f750f629e2578c44d54f7afce
Walter Francis <wally@theblackmoor.net>
2017-10-01 22:21:56 -0400

    Add initial files

---
+++ README.md
@@ -0,0 +1,39 @@
+Edit a file remotely over sshfs
+================
+
+This script makes it easy to locally edit a file over ssh, using sshfs.  Since the script mounts the remote path using sshfs and edits that file directly (no copying the file locally, etc), there should be no issues with symlinks, selinux contexts, etc.  Note that the editor is executed on the _local_ machine.
+
+The script sanity checks the fileurl (ie:  user@machine:/path/to/file) and then breaks it apart and creates a temporary directory in /tmp and uses sshfs to mount the remote path and edits the file.  Along the way many checks are done to ensure that the file exists and is writable.  If the file does not exist it ensures that the path is writable.  At this time the script only warns if the file cannot be written, but fails if the file does not exist and the path cannot be written to.
+
+Specifying the username is optional in the fileurl; if not specified the user is assumed to be the current user.
+
+The full remote file path must be specified, ie:  user@machine:~/fileinhomedir.txt will not work.  The script catches this specific example but there may be others, so always specify the full absolute path to the filename.
+
+The script also supports local files.  If a remote path is not detected, the script will edit the file directly (no sshfs, etc).  You could potentially use this script as your default editor for both local and remote files and not have to run a different editor/script based on the file location.
+
+You can specify which editor you wish to use passing the -e flag into the script which will use the specified editor for this session, ie:  editeremote.sh -e nano user@machine:/path/to/file   Note:  If -e is specified, prompt_for_unmount (see below) is set to 1 to ensure the sshfs isn't unounted too early.
+
+#### Configuration of default editor
+
+To specify a default editor you can create a file named ~/.editremote.sh with two variables in it.
+
+* editor
+  * Path to the editor of your choice
+* prompt_for_unmount
+  * 0 means do not prompt to unount sshfs, it will unmount as soon as the editor exits
+  * 1 means to prompt the user to hit enter to unmount sshfs (for editors which fork to the background)
+
+Example:
+
+editor=sublime 
+prompt_for_unmount=1
+
+#### Use Cases
+
+My primary use case for this script is editing Perl on a remote Raspberry Pi in Sublime so I have all of my favorite plugins and such available without copying files back and forth.  This script can also avoid dealing with latency in a remote shell's editor, however there could be cases where the editor is waiting on data from the remote path, auto-saving, etc.  I have personally not experienced any such issues, however.
+
+#### A few small warnings
+
+First, this creates a temporary path in /tmp to mount the sshfs.  This should not be readable by other system users but since it is in /tmp it is worth mentioning.
+
+Also, if your editor does git stuff, such as querying the current diffs, branches, etc, you might wind up with a git process doing stuff which might cause problems with unmounting the sshfs.  Saving and closing the file from the editor works fine in most cases.  Most modern fancy editors, such as Atom, Sublime, etc, may do this by default.  I have seen Sublime give some strange git-related errors sometimes after allowing the script to unmount sshfs.
---
+++ editremote.sh
@@ -0,0 +1,172 @@
+#!/bin/sh
+
+# Edit a file remotely over sshfs using a defined editor or the one specified with -e
+
+# TODO:  Make sure spaces in path and file are safe and work
+
+# Don't edit this script!  To specify your personal default editor, create a file named
+# ~/.editremote.sh and put these two variables in it with the appropriate values.
+
+# Default editor
+editor=vi
+# Do we wait for the editor proces to return or just prompt the user to hit enter
+# IE:  If your editor forks into the background, set this to 1
+prompt_for_unmount=0
+
+evar=0
+fileurl=""
+localfile=""
+sshfs=0
+
+# Look to see if we have a config file, if so, use it
+if [ -e ~/.editremote.sh ]; then
+    source ~/.editremote.sh
+fi
+
+# Handle -e paramter in any order, ugly but works
+for var in "$@"
+do
+    if [ "$evar" == "1" ]; then
+        editor=$var
+        evar=0
+    fi
+
+    if [ "$var" == "-e" ]; then
+        # If we see -e, the next var is going to be the editor to use
+        evar=1
+        # And to make sure we don't unmount immediately, set the unmount var to 1
+        prompt_for_unmount=1
+    fi
+
+    if [[ "$var" =~ ^(.*):(.*) ]]; then
+        fileurl=$var
+        sshfs=1
+    fi
+
+    # This is goofy, but track a localfile in case we don't wind up detecting a remote file path
+    if [ ${sshfs} == 0 ]; then
+        localfile=$var
+    fi
+done
+
+if [ ! -x "$(type -p $editor)" ]; then
+    echo "${editor} not found in path, exiting"
+    exit
+fi
+
+if [ "$fileurl" == "" ] && [ "$localfile" == "" ]; then
+    echo "No file to edit found; exiting"
+    exit
+fi
+
+# Common function to clean up and exit
+function cleanexit
+{
+    cleandir=$1
+    if [ "$cleandir" != "" ]; then
+        fusermount -uz ${cleandir}
+        rmdir "${cleandir}"
+    fi
+    exit
+}
+
+# Common function to test to make sure we can edit the file or create a new file in this path
+function testedit
+{
+    testfile="$1"
+
+    if [ -e ${testfile} ]; then
+        if [ ! -w "${testfile}" ]; then
+            echo "WARNING!  No permission to edit ${testfile}!"
+            sleep 1s
+        fi
+    else
+        tmpfile=$(mktemp -q ${testfile}.XXXXXXX)
+        touch "${tmpfile}" >/dev/null 2>&1
+        rc=$?
+        if [ $rc != 0 ]; then
+            echo "FAILURE!  No permission to write to path and ${testfile} does not exist!"
+            cleanexit
+        else
+            rm "${tmpfile}"
+        fi
+    fi
+}
+
+if [ $sshfs == 1 ]; then
+    # Make sure sshfs is available
+    if [ ! -x "$(type -p sshfs)" ]; then
+        echo "sshfs is not installed; exiting"
+        exit
+    fi
+
+    # Start breaking up the fileurl
+    user=""
+    url=""
+    fullpath=""
+    dir=""
+    userstring=""
+    remote=""
+
+    # Break up the file url
+    if [[ $fileurl =~ \@ ]]; then
+        user=`echo ${fileurl} | cut -f 1 -d "@"`
+        url=`echo ${fileurl} | cut -f 2 -d "@"`
+    else
+        url=$fileurl
+    fi
+
+    # Now get the directory path out for sshfs
+    remote=`echo ${url} | cut -f 1 -d ":"`
+    fullpath=`echo ${url} | cut -f 2 -d ":"`
+    filename=`basename ${fullpath}`
+    dir=$(dirname "${fullpath}")
+
+    if [[ "$dir" =~ \~ ]]; then
+        echo "Cannot use relative paths such as ~/ please use full path"
+        exit
+    fi
+
+    if [ "$user" != "" ]; then
+        userstring="${user}@"
+    fi
+
+    # Create a somewhat friendly tmp path
+    tmpdirstring=${dir//\//_}
+    tmpdir=`mktemp -d /tmp/XXXX.${remote}${tmpdirstring}`
+
+    # Make sure we have everything we need
+    if [ "$remote" != "" ] && [ "$dir" != "" ] && [ "$filename" != "" ]; then
+        sshfs "${userstring}${remote}:${dir}" "${tmpdir}"
+        rc=$?
+        if [ $rc != 0 ]; then
+            echo "FAILURE!  sshfs failed, invalid hostname or other error?  Attempted the following:"
+            echo "sshfs ${userstring}${remote}:${dir} ${tmpdir}"
+            cleanexit
+        fi
+
+        # Test if we can write to this path and/or file and warn if not
+        remotefile=${tmpdir}/${filename}
+        testedit ${remotefile}
+
+        ${editor} "${remotefile}"
+        pid=$!
+        # We have to handle editors that launch in the the background by watching their pid
+        # But if the editor is already running, like sublime, we get a temporary pid
+        if [ $prompt_for_unmount == 0 ]; then
+            wait ${pid}
+        else
+            # If all else fails, we just prompt to user to let us finish up..  gross
+            echo -n "Hit enter when done editing and file is closed.. "
+            read
+        fi
+        cleanexit "${tmpdir}"
+    else
+        echo "Something went wrong..."
+        echo "remote: [$remote] dir: [$dir] filename: [$filename]"
+        cleanexit "${tmpdir}"
+    fi
+else
+    testedit ${localfile}
+    ${editor} "${localfile}"
+fi
